a1ec5ee8314c701ebef59cb552784311bdb917ac
Eduard Gilmanov <mailnox@mail.ru>
2021-11-29 22:45:28 +0500

    add link

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return(
         <nav>
             <div className="nav-wrapper">
-                <a href="https://github.com/egilmanof/react-movies" className="brand-logo">Поиск фильмов</a>
+                <a href="https://github.com/egilmanof/react-movies" target="_blank" className="brand-logo">Поиск фильмов</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="#">Ссылка на мой репозиторий</a></li>
                 </ul>
